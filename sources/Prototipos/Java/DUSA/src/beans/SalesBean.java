f3b3d1b4bbd0f4884ac3a9b150a42ec645a9bef1
sck25 <sebacaballero@gmail.com>
2014-09-10 14:50:47 -0300

    Pruebas con el datatable para agregar artículos a una venta.

---
+++ sources/Prototipos/Java/DUSA/src/beans/SalesBean.java
@@ -0,0 +1,15 @@
+package beans;
+
+import java.io.Serializable;
+
+import javax.annotation.ManagedBean;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class SalesBean implements Serializable {
+	
+	
+	
+}
