b35a9ffeb99191b55c4b9188def63376187d511d
Ignacio Rodriguez <nachorofa@gmail.com>
2014-09-27 11:21:48 -0300

    cambios en el nueva venta

--- sources/Prototipos/Java/DUSA/src/beans/VentaBean.java
+++ sources/Prototipos/Java/DUSA/src/beans/VentaBean.java
@@ -2,7 +2,6 @@ package beans;
 import java.util.ArrayList;
 import java.util.List;
 
-import model.Articulo;
 import datatypes.DTVenta;
 
 public class VentaBean {
@@ -10,7 +9,7 @@ public class VentaBean {
 private String descripcion = "probandooooooooooo";
 private DTVenta Venta = new DTVenta();
 private List<DTVenta> lineasVenta = new ArrayList<DTVenta>();
-
+private List<DTVenta> lineasVenta2 = new ArrayList<DTVenta>();
 
 public String getDescripcion() {
 	return descripcion;
@@ -18,6 +17,11 @@ public String getDescripcion() {
 
 public void setDescripcion(String descripcion) {
 	this.descripcion = descripcion;
+}
+
+public void agregarItem(DTVenta item) {
+	lineasVenta.remove(item);
+	lineasVenta2.add(item);
 }
 
 //para cargar unos datos para probar es este ini
@@ -43,6 +47,14 @@ public List<DTVenta> getLineasVenta() {
 
 public void setLineasVenta(List<DTVenta> lineasVenta) {
 	this.lineasVenta = lineasVenta;
+}
+
+public List<DTVenta> getLineasVenta2() {
+	return lineasVenta2;
+}
+
+public void setLineasVenta2(List<DTVenta> lineasVenta2) {
+	this.lineasVenta2 = lineasVenta2;
 }
 
 
