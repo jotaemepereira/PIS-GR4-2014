a577925e0b359cbfe083a3e714b44154eddad331
sck25 <sebacaballero@gmail.com>
2014-10-04 10:30:58 -0300

    Fix tabla nueva venta

--- sources/Prototipos/Java/DUSA/src/beans/VentaBean.java
+++ sources/Prototipos/Java/DUSA/src/beans/VentaBean.java
@@ -11,138 +11,147 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
 
 import datatypes.DTVenta;
 
 @ManagedBean
 @ViewScoped
 public class VentaBean implements Serializable {
-	     
-/**
+
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-private String descripcion = "probandooooooooooo";
-private String presentacion = "presentacionprueba";
-private String principioActivo = "pinc. Act prueba";
-private String laboratorio = "Laboratorio prueba";
-private BigDecimal precioVenta = new BigDecimal(0);
+	private String descripcion = "probandooooooooooo";
+	private String presentacion = "presentacionprueba";
+	private String principioActivo = "pinc. Act prueba";
+	private String laboratorio = "Laboratorio prueba";
+	private BigDecimal precioVenta = new BigDecimal(0);
 
-private DTVenta venta = new DTVenta();
-private List<DTVenta> lineasVenta = new ArrayList<DTVenta>();
-private List<DTVenta> lineasVenta2 = new ArrayList<DTVenta>();
-private List<DTVenta> ventasSeleccionadas = new ArrayList<DTVenta>();
+	private DTVenta venta = new DTVenta();
+	private List<DTVenta> lineasVenta = new ArrayList<DTVenta>();
+	private List<DTVenta> lineasVenta2 = new ArrayList<DTVenta>();
+	private List<DTVenta> ventasSeleccionadas = new ArrayList<DTVenta>();
 
-
-public String getPresentacion() {
-	return presentacion;
-}
-
-public void setPresentacion(String presentacion) {
-	this.presentacion = presentacion;
-}
-
-public String getPrincipioActivo() {
-	return principioActivo;
-}
-
-public void setPrincipioActivo(String principioActivo) {
-	this.principioActivo = principioActivo;
-}
-
-public String getLaboratorio() {
-	return laboratorio;
-}
-
-public void setLaboratorio(String laboratorio) {
-	this.laboratorio = laboratorio;
-}
-
-public BigDecimal getPrecioVenta() {
-	return precioVenta;
-}
-
-public void setPrecioVenta(BigDecimal precioVenta) {
-	this.precioVenta = precioVenta;
-}
-
-public String getDescripcion() {
-	return descripcion;
-}
-
-public void setDescripcion(String descripcion) {
-	this.descripcion = descripcion;
-}
-
-public void agregarLineasVenta() {
-	//aca en realidad hay q buscar las ventas con el buscarArticulo y agregar todos los que coinciden con la descripcion buscados
-	for (int i=0; i<4; i++) {
-		DTVenta dt = new DTVenta();
-		dt.setCantidad(i);
-		dt.setDescripcion(descripcion);
-		dt.setLaboratorio(laboratorio);
-		dt.setPrecioVenta(precioVenta);
-		dt.setPresentacion(presentacion);
-		dt.setPrincipioActivo(principioActivo);
-		lineasVenta.add(dt);
+	public String getPresentacion() {
+		return presentacion;
 	}
-}
 
-//este agregar es para agregar los productos buscados a la venta
-public void agregarVenta(){
+	public void setPresentacion(String presentacion) {
+		this.presentacion = presentacion;
+	}
+
+	public String getPrincipioActivo() {
+		return principioActivo;
+	}
+
+	public void setPrincipioActivo(String principioActivo) {
+		this.principioActivo = principioActivo;
+	}
+
+	public String getLaboratorio() {
+		return laboratorio;
+	}
+
+	public void setLaboratorio(String laboratorio) {
+		this.laboratorio = laboratorio;
+	}
+
+	public BigDecimal getPrecioVenta() {
+		return precioVenta;
+	}
+
+	public void setPrecioVenta(BigDecimal precioVenta) {
+		this.precioVenta = precioVenta;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public void agregarLineasVenta() {
+		// aca en realidad hay q buscar las ventas con el buscarArticulo y
+		// agregar todos los que coinciden con la descripcion buscados
+		for (int i = 0; i < 4; i++) {
+			DTVenta dt = new DTVenta();
+			dt.setCantidad(i);
+			dt.setDescripcion(descripcion);
+			dt.setLaboratorio(laboratorio);
+			dt.setPrecioVenta(precioVenta);
+			dt.setPresentacion(presentacion);
+			dt.setPrincipioActivo(principioActivo);
+			lineasVenta.add(dt);
+		}
+	}
+
+	// este agregar es para agregar los productos buscados a la venta
 	
-	Iterator<DTVenta> it = ventasSeleccionadas.iterator();
-	while (it.hasNext()){
-		DTVenta dt = new DTVenta();
-		lineasVenta2.add(dt);
-		dt = it.next();
+	public void agregarVenta(ActionEvent actionEvent) {
+
+		List<DTVenta> removidas = new ArrayList<DTVenta>();
+		
+		Iterator<DTVenta> it = ventasSeleccionadas.iterator();
+		while (it.hasNext()) {
+			DTVenta d = it.next();
+			lineasVenta2.add(d);
+			removidas.add(d);
+		}
+		
+		ventasSeleccionadas = new ArrayList<DTVenta>();
+		
+		for (DTVenta d : removidas){
+			lineasVenta.remove(d);
+		}
 	}
-}
 
-public String strTotal(){
-	BigDecimal total = new BigDecimal(0);
-	Iterator<DTVenta> it = lineasVenta2.iterator();
-	while (it.hasNext()){
-		total = total.add(it.next().getPrecioVenta());
+	public String strTotal() {
+		BigDecimal total = new BigDecimal(0);
+		Iterator<DTVenta> it = lineasVenta2.iterator();
+		while (it.hasNext()) {
+			total = total.add(it.next().getPrecioVenta());
+		}
+		return total.toString();
 	}
-	return total.toString();
-}
 
-public List<DTVenta> getLineasVenta() {
-	return lineasVenta;
-}
+	public List<DTVenta> getLineasVenta() {
+		return lineasVenta;
+	}
 
-public void setLineasVenta(List<DTVenta> lineasVenta) {
-	this.lineasVenta = lineasVenta;
-}
+	public void setLineasVenta(List<DTVenta> lineasVenta) {
+		this.lineasVenta = lineasVenta;
+	}
 
-public List<DTVenta> getLineasVenta2() {
-	return lineasVenta2;
-}
+	public List<DTVenta> getLineasVenta2() {
+		return lineasVenta2;
+	}
 
-public void setLineasVenta2(List<DTVenta> lineasVenta2) {
-	this.lineasVenta2 = lineasVenta2;
-}
+	public void setLineasVenta2(List<DTVenta> lineasVenta2) {
+		this.lineasVenta2 = lineasVenta2;
+	}
 
-public DTVenta getVenta() {
-	return venta;
-}
+	public DTVenta getVenta() {
+		return venta;
+	}
 
-public void setVenta(DTVenta venta) {
-	this.venta = venta;
-}
+	public void setVenta(DTVenta venta) {
+		this.venta = venta;
+	}
 
-public List<DTVenta> getVentasSeleccionadas() {
-	return ventasSeleccionadas;
-}
+	public List<DTVenta> getVentasSeleccionadas() {
+		return ventasSeleccionadas;
+	}
 
-public void setVentasSeleccionadas(List<DTVenta> ventasSeleccionadas) {
-	this.ventasSeleccionadas = ventasSeleccionadas;
-}
+	public void setVentasSeleccionadas(List<DTVenta> ventasSeleccionadas) {
+		this.ventasSeleccionadas = ventasSeleccionadas;
+	}
 
-public VentaBean(){
-	agregarLineasVenta();
-}
-
+	public VentaBean() {
+		agregarLineasVenta();
+	}
 
 }
