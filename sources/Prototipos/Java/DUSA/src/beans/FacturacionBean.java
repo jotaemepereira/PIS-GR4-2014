c1ae45108c2f60acb201b82453fa9f7be360f3dd
sck25 <sebacaballero@gmail.com>
2014-10-07 20:02:58 -0300

    -

--- sources/Prototipos/Java/DUSA/src/beans/FacturacionBean.java
+++ sources/Prototipos/Java/DUSA/src/beans/FacturacionBean.java
@@ -3,9 +3,11 @@ package beans;
 import interfaces.IFacturacion;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Random;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
@@ -20,6 +22,7 @@ import model.Venta;
 public class FacturacionBean implements Serializable {
 
 	private List<Venta> ventas;
+	private Venta ventaSeleccionada;
 
 	public FacturacionBean() {
 		try {
@@ -35,11 +38,11 @@ public class FacturacionBean implements Serializable {
 
 	public void facturar(Venta v) {
 		try {
-//			IFacturacion ifact = FabricaLogica.getIFacturacion();
-//			ifact.facturarVenta(v.getVentaId());
-			
+			// IFacturacion ifact = FabricaLogica.getIFacturacion();
+			// ifact.facturarVenta(v.getVentaId());
+
 			ventas.remove(v);
-			
+
 			FacesContext.getCurrentInstance().addMessage(
 					null,
 					new FacesMessage(FacesMessage.SEVERITY_INFO,
@@ -52,9 +55,36 @@ public class FacturacionBean implements Serializable {
 		}
 	}
 	
-	public String parseFechaVenta(Date d){
-		SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
-		return formatter.format(d);
+	public void cargarFactura(Venta v){
+		ventaSeleccionada = v;
+	}
+
+	public String parseFechaVenta(Date d) {
+		// long diff = (new Date()).getTime() - d.getTime();
+		// return "" + (diff / (60 * 1000));
+		Random r = new Random();
+		int a = Math.abs(r.nextInt() % 15);
+		if (a == 0) {
+			a = 1;
+		}
+		return "Hace " + a + " minutos";
+	}
+
+	public String obtenerNombre(BigDecimal precio) {
+		// sacar
+		if (precio.intValue() == 25){
+			return "Producto1";
+		} else if (precio.intValue() == 15) {
+			return "Producto2";
+		} else if (precio.intValue() == 45) {
+			return "Producto3";
+		} else {
+			return "Producto4";
+		}
+	}
+
+	public BigDecimal calculcarSubtotal(int cantidad, BigDecimal precio){
+		return precio.multiply(new BigDecimal(cantidad));
 	}
 
 	public List<Venta> getVentas() {
@@ -63,6 +93,14 @@ public class FacturacionBean implements Serializable {
 
 	public void setVentas(List<Venta> ventas) {
 		this.ventas = ventas;
+	}
+
+	public Venta getVentaSeleccionada() {
+		return ventaSeleccionada;
+	}
+
+	public void setVentaSeleccionada(Venta ventaSeleccionada) {
+		this.ventaSeleccionada = ventaSeleccionada;
 	}
 
 }
