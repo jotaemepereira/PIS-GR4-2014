370bcd86ed6d1bcdc9f80ac4a061f6b8b662203e
guille <galleplop@gmail.com>
2014-10-18 15:07:03 -0200

    [wsimport] Pedido con cambios en objectFactory.

---
+++ sources/Prototipos/Java/DUSA/src/uy/com/dusa/ws/MensajeError.java
@@ -0,0 +1,98 @@
+
+package uy.com.dusa.ws;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for mensajeError complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="mensajeError">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="codigo" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *         &lt;element name="mensaje" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="valor" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "mensajeError", propOrder = {
+    "codigo",
+    "mensaje",
+    "valor"
+})
+public class MensajeError {
+
+    protected int codigo;
+    protected String mensaje;
+    protected int valor;
+
+    /**
+     * Gets the value of the codigo property.
+     * 
+     */
+    public int getCodigo() {
+        return codigo;
+    }
+
+    /**
+     * Sets the value of the codigo property.
+     * 
+     */
+    public void setCodigo(int value) {
+        this.codigo = value;
+    }
+
+    /**
+     * Gets the value of the mensaje property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    /**
+     * Sets the value of the mensaje property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setMensaje(String value) {
+        this.mensaje = value;
+    }
+
+    /**
+     * Gets the value of the valor property.
+     * 
+     */
+    public int getValor() {
+        return valor;
+    }
+
+    /**
+     * Sets the value of the valor property.
+     * 
+     */
+    public void setValor(int value) {
+        this.valor = value;
+    }
+
+}
---
+++ sources/Prototipos/Java/DUSA/src/uy/com/dusa/ws/ResultRealizarPedido.java
@@ -0,0 +1,60 @@
+
+package uy.com.dusa.ws;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for resultRealizarPedido complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="resultRealizarPedido">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="mensaje" type="{http://ws.dusa.com.uy/}mensajeError" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "resultRealizarPedido", propOrder = {
+    "mensaje"
+})
+public class ResultRealizarPedido {
+
+    protected MensajeError mensaje;
+
+    /**
+     * Gets the value of the mensaje property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link MensajeError }
+     *     
+     */
+    public MensajeError getMensaje() {
+        return mensaje;
+    }
+
+    /**
+     * Sets the value of the mensaje property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link MensajeError }
+     *     
+     */
+    public void setMensaje(MensajeError value) {
+        this.mensaje = value;
+    }
+
+}
