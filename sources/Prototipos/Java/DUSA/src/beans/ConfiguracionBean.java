6bb2ba0925757ebcb0886e4ef9b440fa22eb755e
jotaemepereira <juanmanuel.pereira1@gmail.com>
2014-10-18 19:43:29 -0200

    Subo arcivho de configuracion para las imagenes

--- sources/Prototipos/Java/DUSA/src/beans/ConfiguracionBean.java
+++ sources/Prototipos/Java/DUSA/src/beans/ConfiguracionBean.java
@@ -1,18 +1,10 @@
 package beans;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.Serializable;
 
-import javax.faces.application.FacesMessage;
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
-import javax.servlet.http.Part;
-
-import org.primefaces.model.UploadedFile;
 
 @ManagedBean
 @ApplicationScoped
