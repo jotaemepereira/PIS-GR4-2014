9e7e0696c45688413ece686302ad3bdcb2ff6759
jotaemepereira <juanmanuel.pereira1@gmail.com>
2014-10-18 23:07:35 -0200

    Cambios en el login.xhtml y loginBean Agrego algunas validaciones en el Login y hago algunas pruebas

--- sources/Prototipos/Java/DUSA/src/beans/ConfiguracionBean.java
+++ sources/Prototipos/Java/DUSA/src/beans/ConfiguracionBean.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 
 @ManagedBean
@@ -11,13 +13,19 @@ import javax.faces.context.FacesContext;
 public class ConfiguracionBean implements Serializable {
 
 	private String rutaLogo;
-
+	//@ManagedProperty("#{loginBean.nomUsuario}")
+    //private LoginBean nomUsuario;
+	
 	public ConfiguracionBean() {
 		setRutaLogo(FacesContext.getCurrentInstance().getExternalContext()
 				.getInitParameter("IMAGEN"));
 	}
 
-
+	/*public void setActiveUser(LoginBean login) {
+        this.nomUsuario = login;
+    }*/
+	
+	
 	public String getRutaLogo() {
 		return rutaLogo;
 	}
--- sources/Prototipos/Java/DUSA/src/beans/LoginBean.java
+++ sources/Prototipos/Java/DUSA/src/beans/LoginBean.java
@@ -1,12 +1,17 @@
 package beans;
 
+import java.io.IOException;
 import java.io.Serializable;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 
 /**
- * @author santiago
+ * @author santiago, juan
  */
+@ManagedBean
+@SessionScoped
 public class LoginBean implements Serializable {
 
 
@@ -15,6 +20,7 @@ public class LoginBean implements Serializable {
 	long idUsuario;
 	String nomUsuario;
 	String contrasenia;
+	
 	public long getIdUsuario() {
 		return idUsuario;
 	}
@@ -24,11 +30,26 @@ public class LoginBean implements Serializable {
 	public String getContrasenia() {
 		return contrasenia;
 	}
+	
+	public String getNomUsuario() {
+		return nomUsuario;
+	}
+	public void setNomUsuario(String nomUsuario) {
+		this.nomUsuario = nomUsuario;
+	}
 	public void setContrasenia(String contrasenia) {
 		this.contrasenia = contrasenia;
 	}
 	public void iniciarSesion(){
 		
+		if ((nomUsuario.compareTo("admin") == 0) && (contrasenia.compareTo("1234") == 0)) {
+			try {
+				FacesContext.getCurrentInstance().getExternalContext().redirect("http://localhost:8080/DUSA/stock/altaArticulo.jsf");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 	public void cerrarSesion(){
 		
