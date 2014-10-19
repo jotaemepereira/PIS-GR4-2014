4583989aef8451bfe583e65d0c6138d3b83bcee3
guille <galleplop@gmail.com>
2014-10-19 00:21:14 -0200

    [generarPedido] Envio de datos a DUSA

--- sources/Prototipos/Java/DUSA/src/controladores/ServicioDusaControlador.java
+++ sources/Prototipos/Java/DUSA/src/controladores/ServicioDusaControlador.java
@@ -1,9 +1,19 @@
 package controladores;
 import java.net.URL;
+import java.util.Iterator;
 
 import javax.xml.namespace.QName;
 
+import uy.com.dusa.ws.DataLineaPedidoSimple;
+import uy.com.dusa.ws.DataPedidoSimple;
+import uy.com.dusa.ws.MensajeError;
+import uy.com.dusa.ws.PedidoFormaDePago;
 import uy.com.dusa.ws.PedidoObjectFactory;
+import uy.com.dusa.ws.ResultRealizarPedido;
+import uy.com.dusa.ws.WSPedidos;
+import uy.com.dusa.ws.WSPedidosService;
+import model.Enumerados.TipoFormaDePago;
+import model.LineaPedido;
 import model.Pedido;
 import interfaces.IServicio;
 
@@ -29,9 +39,32 @@ public class ServicioDusaControlador implements IServicio {
 		
 		try {
 			
-			URL url = new URL(dusaComprobantesURL);
-			
+//			URL url = new URL(dusaComprobantesURL);
 //			QName qName = new PedidoObjectFactory();
+			DataPedidoSimple dPedido = new DataPedidoSimple();
+			if (p.getFormaDePago() == TipoFormaDePago.CONTADO) {
+				
+				dPedido.setFormaDePago(PedidoFormaDePago.CONTADO);
+			} else {
+
+				dPedido.setFormaDePago(PedidoFormaDePago.CREDITO);
+			}
+			
+			for (Iterator<LineaPedido> iterator = p.getLineas().iterator(); iterator.hasNext();) {
+				
+				LineaPedido lPedido = iterator.next();
+				
+				DataLineaPedidoSimple dlPedido = new DataLineaPedidoSimple();
+				dlPedido.setNumeroArticulo(lPedido.getNumeroArticulo().intValue());
+				dlPedido.setCantidad(lPedido.getCantidad());
+				
+				dPedido.getLineas().add(dlPedido);
+			}
+			
+			WSPedidos wsPedido = new WSPedidosService().getWSPedidosPort();
+			ResultRealizarPedido resPedido = wsPedido.realizarPedidoSimple(userTest, passTest, dPedido);
+			
+			MensajeError error = resPedido.getMensaje();
 			
 			
 		} catch (Exception e) {
