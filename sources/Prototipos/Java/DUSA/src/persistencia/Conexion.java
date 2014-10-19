15366c939d319bb4bcd07c0d198cc37806ed2662
Santiago <santiago.taba@gmail.com>
2014-10-19 00:15:08 -0200

    ESTO NUNCA PASO... EL REGRESO DE LA CONEXION

--- sources/Prototipos/Java/DUSA/src/persistencia/Conexion.java
+++ sources/Prototipos/Java/DUSA/src/persistencia/Conexion.java
@@ -10,34 +10,35 @@ import javax.naming.NamingException;
 public class Conexion {
 	public static Connection getConnection() throws NamingException,
 			SQLException {
-//		try {
-//			Class.forName(FacesContext.getCurrentInstance()
-//					.getExternalContext().getInitParameter("DRIVER"));
-//		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-//		}
-
-//		String user = "root";
-//		FacesContext.getCurrentInstance().getExternalContext().getInitParameter("DBUSER");
-//		String password = "root";
-//		FacesContext.getCurrentInstance().getExternalContext().getInitParameter("DBPWD");
-//		String connection = FacesContext.getCurrentInstance().getExternalContext().getInitParameter("CONNSTRING");
-		
-		
-		return DriverManager.getConnection("jdbc:postgresql://localhost:5432/pisgr4", "root", "root");
-		
-		/*
-		
 		try {
- 			Class.forName("org.postgresql.Driver");
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-		}
+            Class.forName(FacesContext.getCurrentInstance()
+                    .getExternalContext().getInitParameter("DRIVER"));
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+        }
 
-		String user = "root";
-		String password = "root";
-		String connection = "jdbc:postgresql://localhost:5432/pisgr4";
-		return DriverManager.getConnection(connection, user, password);*/
+        String user = FacesContext.getCurrentInstance().getExternalContext()
+                .getInitParameter("DBUSER");
+        String password = FacesContext.getCurrentInstance()
+                .getExternalContext().getInitParameter("DBPWD");
+        String connection = FacesContext.getCurrentInstance()
+                .getExternalContext().getInitParameter("CONNSTRING");
+        return DriverManager.getConnection(connection, user, password);
+        /*
+        
+        try {
+             Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+        }
+
+        String user = "root";
+        String password = "root";
+        String connection = "jdbc:postgresql://localhost:5432/pisgr4";
+        return DriverManager.getConnection(connection, user, password);*/
+		
+	//	return DriverManager.getConnection("jdbc:postgresql://localhost:5432/pisgr4", "root", "root");
+		
 	}
 
 }
