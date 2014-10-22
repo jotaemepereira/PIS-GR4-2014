1cbc24117fe3e2d13f215aecbe0145faf7125857
Victoria-Diaz <mavidiaz282@gmail.com>
2014-10-22 17:00:32 -0200

    se continuó con la interfaz y bean de ingresar compra

--- sources/Prototipos/Java/DUSA/src/beans/ComprasBean.java
+++ sources/Prototipos/Java/DUSA/src/beans/ComprasBean.java
@@ -11,8 +11,75 @@ import datatypes.DTBusquedaArticulo;
 import datatypes.DTProveedor;
 
 public class ComprasBean implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Boolean disableBotones = false;
+	private String hideTable = "hidden";
 	
 	private List<DTProveedor> busquedaProveedores;
 	private List<DTBusquedaArticulo> busquedaArticulos;
+	
+	
+	// getters y setters
+	public Boolean getDisableBotones() {
+		return disableBotones;
+	}
+
+	public void setDisableBotones(Boolean disableBotones) {
+		this.disableBotones = disableBotones;
+	}
+
+	public String getHideTable() {
+		return hideTable;
+	}
+
+	public void setHideTable(String hideTable) {
+		this.hideTable = hideTable;
+	}
+
+	public List<DTProveedor> getBusquedaProveedores() {
+		return busquedaProveedores;
+	}
+
+	public void setBusquedaProveedores(List<DTProveedor> busquedaProveedores) {
+		this.busquedaProveedores = busquedaProveedores;
+	}
+
+	public List<DTBusquedaArticulo> getBusquedaArticulos() {
+		return busquedaArticulos;
+	}
+
+	public void setBusquedaArticulos(List<DTBusquedaArticulo> busquedaArticulos) {
+		this.busquedaArticulos = busquedaArticulos;
+	}
+	
+	
+	
+	// funciones ingresar compra
+	
+	public void ingresoManual(){
+		disableBotones = true;
+		hideTable = "visible";
+	}
+	
+	public void facturaAutomáticaDUSA(){
+		disableBotones = true;
+		hideTable = "visible";
+	}
+	
+	public void cancelarIngresarCompra(){
+		disableBotones = false;
+		hideTable = "hidden";
+	}
+
+	public void ingresarCompra(){
+		
+		
+		// Reseteo los valores por defecto
+		disableBotones = false;
+		hideTable = "hidden";
+	}
+	
+	
 
 }
