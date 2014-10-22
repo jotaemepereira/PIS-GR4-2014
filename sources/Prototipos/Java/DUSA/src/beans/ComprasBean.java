f3413f27856e4f348a249a14cd479776195ac34e
Victoria-Diaz <mavidiaz282@gmail.com>
2014-10-22 17:45:31 -0200

    [INGRESAR COMPRA] se avanzó con la interfaz

--- sources/Prototipos/Java/DUSA/src/beans/ComprasBean.java
+++ sources/Prototipos/Java/DUSA/src/beans/ComprasBean.java
@@ -5,20 +5,30 @@
 package beans;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
 import datatypes.DTBusquedaArticulo;
+import datatypes.DTComprobanteFactura;
 import datatypes.DTProveedor;
 
+@ManagedBean
+@SessionScoped
 public class ComprasBean implements Serializable {
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 3L;
 
 	private Boolean disableBotones = false;
 	private String hideTable = "hidden";
+	private String selectFacturaDUSA = "hidden";
 	
 	private List<DTProveedor> busquedaProveedores;
 	private List<DTBusquedaArticulo> busquedaArticulos;
-	
+	private List<DTComprobanteFactura> facturasDUSA = new ArrayList<DTComprobanteFactura>();
+	private long ordenDeCompraDUSA;
+	private DTComprobanteFactura factura = new DTComprobanteFactura();
 	
 	// getters y setters
 	public Boolean getDisableBotones() {
@@ -53,23 +63,56 @@ public class ComprasBean implements Serializable {
 		this.busquedaArticulos = busquedaArticulos;
 	}
 	
+	public List<DTComprobanteFactura> getFacturasDUSA() {
+		return facturasDUSA;
+	}
+
+	public void setFacturasDUSA(List<DTComprobanteFactura> facturasDUSA) {
+		this.facturasDUSA = facturasDUSA;
+	}
+
+	public DTComprobanteFactura getFactura() {
+		return factura;
+	}
+
+	public void setFactura(DTComprobanteFactura factura) {
+		this.factura = factura;
+	}
 	
+	public long getOrdenDeCompraDUSA() {
+		return ordenDeCompraDUSA;
+	}
+
+	public void setOrdenDeCompraDUSA(long ordenDeCompraDUSA) {
+		this.ordenDeCompraDUSA = ordenDeCompraDUSA;
+	}
+	
+	public String getSelectFacturaDUSA() {
+		return selectFacturaDUSA;
+	}
+
+	public void setSelectFacturaDUSA(String selectFacturaDUSA) {
+		this.selectFacturaDUSA = selectFacturaDUSA;
+	}
 	
 	// funciones ingresar compra
 	
 	public void ingresoManual(){
 		disableBotones = true;
 		hideTable = "visible";
+		selectFacturaDUSA = "hidden";
 	}
 	
 	public void facturaAutomáticaDUSA(){
 		disableBotones = true;
 		hideTable = "visible";
+		selectFacturaDUSA = "visible";
 	}
 	
 	public void cancelarIngresarCompra(){
 		disableBotones = false;
 		hideTable = "hidden";
+		selectFacturaDUSA = "hidden";
 	}
 
 	public void ingresarCompra(){
@@ -78,8 +121,8 @@ public class ComprasBean implements Serializable {
 		// Reseteo los valores por defecto
 		disableBotones = false;
 		hideTable = "hidden";
+		selectFacturaDUSA = "hidden";
 	}
-	
-	
+
 
 }
