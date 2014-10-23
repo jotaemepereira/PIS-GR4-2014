805896f6f4fe040f3d79742a7eecdedc119cc796
Jmaguerre <jmaguerre@gmail.com>
2014-10-23 17:42:29 -0200

    Arreglo de errores en altaArticulo.

---
+++ sources/Prototipos/Java/DUSA/src/Util/StringTrimmer.java
@@ -0,0 +1,21 @@
+package Util;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+@FacesConverter(forClass=String.class)
+public class StringTrimmer implements Converter {
+
+    @Override
+    public Object getAsObject(FacesContext context, UIComponent component, String value) {
+        return value != null ? value.trim() : null;
+    }
+
+    @Override
+    public String getAsString(FacesContext context, UIComponent component, Object value) {
+        return (String) value;
+    }
+
+}
