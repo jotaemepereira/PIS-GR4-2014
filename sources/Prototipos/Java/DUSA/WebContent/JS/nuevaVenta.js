373f11bd39edf96fe1bfab1fda16a39bb7134a5c
sck25 <sebacaballero@gmail.com>
2014-11-16 18:46:38 -0200

    [NuevaVenta] Agrego select a cantidad cuando se abre el dialog.

--- sources/Prototipos/Java/DUSA/WebContent/JS/nuevaVenta.js
+++ sources/Prototipos/Java/DUSA/WebContent/JS/nuevaVenta.js
@@ -6,10 +6,16 @@ function keypressBusqueda(e) {
 	}
 }
 
-function keypressLector(e){
+function keypressLector(e) {
 	if (event.keyCode == 13) {
 		e.preventDefault();
 		buscarArticuloLector();
 		return false;
 	}
+}
+
+function selectCantidad() {
+	var myInput = document.getElementById('ventaDetail:cant');
+	myInput.focus();
+	myInput.select()
 }
